be91955ccb3a90adfe76f9bd6ff31865378c94ad
jonathandemario <c14210001@john.petra.ac.id>
2024-06-26 20:01:34 +0700

    create detail page

---
+++ lib/detail_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:note_taking_app/note.dart';
+import 'package:note_taking_app/main.dart';
+import 'package:note_taking_app/home_page.dart';
+import 'package:intl/intl.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class DetailPage extends StatefulWidget {
+  final int curr_index;
+  final String menuType;
+
+  DetailPage({
+    super.key,
+    required this.curr_index,
+    required this.menuType
+  });
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  var _notes = Hive.box<Note>('notes');
+  List<Note> noteList = [];
+
+  // Future fetchNotes() async {
+  //   final keyList = _notes.keys.toList();
+  //   if (keyList.isNotEmpty) {
+  //     for (var key in keyList) {
+  //       print ('Key: ${key}');
+  //       print(_notes.get(key)!.noteTitle);
+  //       // _notes.delete(key);
+  //       setState(() {
+  //         noteList.add(_notes.get(key)!);
+  //       });
+  //     }
+  //   } else {
+  //     print('Notes Empty');
+  //   }
+  // }
+
+  int checkIndex() {
+    if (_notes.isEmpty) {
+      return 0;
+    }
+    int maxKey = _notes.keys.reduce((a, b) => a > b ? a : b);
+    return maxKey + 1;
+  }
+
+  String getTime() {
+    DateTime curr_timestamp = DateTime.now();
+    String curr_time = DateFormat('dd/MM/yyyy HH:mm:ss').format(curr_timestamp);
+    return curr_time;
+  }
+
+  // Note this_data = Note(noteName: 'c', noteDetail: 'abcdefghij', noteCreatedDate: getTime(), noteUpdatedDate: getTime());
+  // _notes.put(checkIndex(), this_data);
+  Future createNote(data) async {
+    _notes.put(checkIndex(), data);
+  }
+
+  Future editNote(index, title, detail, updatedTime) async {
+    Note updated_note = _notes.get(index)!;
+
+    updated_note.noteTitle = title;
+    updated_note.noteDetail = detail;
+    updated_note.noteUpdatedDate = updatedTime;
+
+    _notes.put(index, updated_note);
+  }
+
+  Future deleteNote(index) async {
+    _notes.delete(index);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Note'),
+      ),
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/note.dart';
 import 'package:note_taking_app/main.dart';
+import 'package:note_taking_app/detail_page.dart';
 import 'package:intl/intl.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -13,58 +14,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var _notes = Hive.box<Note>('notes');
+  List<Note> noteList = [];
 
   Future fetchNotes() async {
     final keyList = _notes.keys.toList();
-
-    print('This all notes:');
     if (keyList.isNotEmpty) {
       for (var key in keyList) {
-        print ('Key: ${key}');
-        print(_notes.get(key)!.noteTitle);
-        _notes.delete(key);
+        print ('Key: ${key}\n${_notes.get(key)!.noteTitle}');
+        setState(() {
+          noteList.add(_notes.get(key)!);
+        });
       }
     } else {
       print('Notes Empty');
     }
   }
 
-  int checkIndex() {
-    if (_notes.isEmpty) {
-      return 0;
-    }
-    int maxKey = _notes.keys.reduce((a, b) => a > b ? a : b);
-    return maxKey + 1;
-  }
-
-  String getTime() {
-    DateTime curr_timestamp = DateTime.now();
-    String curr_time = DateFormat('dd/MM/yyyy HH:mm:ss').format(curr_timestamp);
-    return curr_time;
-  }
-
-  // Note this_data = Note(noteName: 'c', noteDetail: 'abcdefghij', noteCreatedDate: getTime(), noteUpdatedDate: getTime());
-  // _notes.put(checkIndex(), this_data);
-  void createNote(data) {
-    _notes.put(checkIndex(), data);
-  }
-
-  void editNote(index, title, detail, updatedTime) {
-    Note updated_note = _notes.get(index)!;
-
-    updated_note.noteTitle = title;
-    updated_note.noteDetail = detail;
-    updated_note.noteUpdatedDate = updatedTime;
-
-    _notes.put(index, updated_note);
-  }
-
-  void deleteNote(index) {
-    _notes.delete(index);
-  }
-
   @override
   void initState() {
+    super.initState();
     fetchNotes();
   }
 
